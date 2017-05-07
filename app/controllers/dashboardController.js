d7659d94d4120490057bce2ad9cd9bcda389839e
Peter K <napstegucio124@googlemail.com>
2017-05-07 14:18:34 +0100

    Trying to add appointments in to the calendar

--- app/controllers/dashboardController.js
+++ app/controllers/dashboardController.js
@@ -3,22 +3,17 @@
     
     var app = angular.module('myApp');
     
-    app.controller('dashboardController', ['$scope', function($scope) {
+    app.controller('dashboardController', ['$scope', 'appointmentsService', function($scope, appointmentsService) {
         
     $scope.Date = new Date();
     $scope.currentView = "week";
+ 
+    appointmentsService.getAppointments().then(function (results) {
+        $scope.appointments = Object.values(results.data);
+        console.log($scope.appointments);
+    });
         
-    var appointmentsCollection = [{
-        Id: 1, 
-        Subject: "Bering Sea Gold", 
-        StartTime: new Date(2017, 4, 5, 5, 30), 
-        EndTime: new Date(2017, 4, 5, 7, 30), 
-        Description:"", 
-        AllDay: false, 
-        Recurrence: false
-    }];
-        
-    $scope.appointments = appointmentsCollection;
+   
         
     function onAppointmentSave(args) {
         console.log("appointment was saved");
--- app/services/appointmentsService.js
+++ app/services/appointmentsService.js
@@ -1,17 +1,18 @@
 (function () {
     
     'use strict';
+    
+    var app = angular.module("myApp");
+    
     app.factory('appointmentsService', ['$http', function($http) {
         
-        var serviceBase = '';
+        var serviceBase = 'http://localhost:63185/';
         var appointmentsServiceFactory = {};
         
         var _getAppointments = function () {
-            
-            return $http.get(serviceBase + 'api/appointments')
-                .then(function (results) {
-                    return results;
-            });
+             return $http.get(serviceBase + 'api/appointments').then(function (results) {
+                 return results;
+             });
         };
         
         appointmentsServiceFactory.getAppointments = _getAppointments;
